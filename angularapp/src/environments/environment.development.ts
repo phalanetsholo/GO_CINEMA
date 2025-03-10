09564b15ae66dff0580a81aa59a61ff80a70394b
phalanetsholo <phalanetsholo@gmail.com>
2025-03-10 11:14:16 +0200

    addeda environments

---
+++ angularapp/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    tmdbApiKey: '65f34dc45ab97e68cb1b6074eb8b781f',
+}
---
+++ angularapp/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    tmdbApiKey: '65f34dc45ab97e68cb1b6074eb8b781f',
+}
